c05c4d78cfbcd3d63efb96e604d57139ef7d1dbc
Nana Tel <Nana Tel>
2016-04-03 12:45:19 -0400

    access controll

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http= require("http");
 var port = process.env.PORT || 3000;
 
 var server = http.createServer(function(req,res){
+	res.setHeader('Access-Control-Allow-Origin', '*');
 res.end("hello");
 });
 
