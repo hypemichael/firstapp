e8a5704f96e0cbe471177c96d9869fa342d4b6f1
hypemichael <hypemichael@yahoo.com>
2016-04-12 15:30:50 -0400

    testing xml

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-var http= require("http");
+
+var http= require("http"); 
 var port = process.env.PORT || 3000;
+var request = require('request');
+xml2js = require('xml2js');
+ 
+var parser = new xml2js.Parser({explicitArray : false});
 
-var server = http.createServer(function(req,res){
-	res.setHeader('Access-Control-Allow-Origin', '*');
-res.end("react/index.html");
-});
-
-server.listen(port);
+ 
+http.createServer(function(req, res) {  
+  var url = 'http://pass.gear.host/Social_Studies.xml';
+  
+  request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+	  parser.parseString(body, function (err, result) {
+        res.end(JSON.stringify(result));
+    });
+  
+  }
+  });
+}).listen(port);
